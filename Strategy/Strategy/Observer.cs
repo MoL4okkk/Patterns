b07653eb722bee4e977ce911f9495fe2d833fe1f
MoL4okkk <molchok.yurij@gmail.com>
2019-12-13 14:39:35 +0300

    Наблюдатель

---
+++ Strategy/Strategy/Observer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Observer
+    {
+        private IStrategy Start;
+        private IStrategy Finish;
+
+        public void ChangeUpdate(object Object, EventArgs e)
+        {
+            IStrategy[] strategies = Object as IStrategy[];
+            Update(strategies[0], strategies[1]);
+        }
+
+        private void Update(IStrategy Start, IStrategy Finish)
+        {
+            this.Start = Start;
+            this.Finish = Finish;
+        }
+    }
+}
